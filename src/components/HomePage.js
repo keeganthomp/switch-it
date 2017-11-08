60406af0a7eb344666786d47f5d87ceb21b690c8
keeganthomp <whereiskeegan@gmail.com>
2017-11-07 18:21:47 -0600

    inital commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bank App</h1>
+
+      <h2>Let's Transfer shit</h2>
+      <h4>Let's Get Started</h4>
+    </div>
+  );
+};
+
+export default HomePage;
